8facf511b49d21bf0c0718db2e8497ada292ae9e
AHMET-CHILL <101750202+AHMET-CHILL@users.noreply.github.com>
2024-07-30 12:42:17 +0300

    first commit

---
+++ src/test/java/com/ch/ll/booking_application/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch.ll.booking_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
